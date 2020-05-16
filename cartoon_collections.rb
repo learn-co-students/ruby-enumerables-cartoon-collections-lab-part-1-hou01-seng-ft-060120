d257d40d02f91ba518306d68ca59576174820f8d
Niketa Jenerette <niketa.jenerette@gmail.com>
2020-05-16 06:53:00 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,11 +1,21 @@
 def greet_characters(array)
+  array.each do |element|
+    puts "Hello #{element}!}"
+  end
+end
+  
   # Use `each` to enumerate over the provided array
   #
   # Print a custom greeting for each element
-end
+
 
 def list_dwarves(array)
+  array.each_with_index{|element, index| 
+  indexplusone= index+1 
+  puts "#{indexplusone}.*#{element}"}
+  
+end
+  
   # Use `each_with_index` to enumerate over the provided array
   #
   # Print a numbered list of each element
-end
